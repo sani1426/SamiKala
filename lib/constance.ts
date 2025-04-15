108df9bd002f7d6f5e404d665f72ea31758f2abf
saman <sam.karimi727@gmail.com>
2025-04-16 01:28:57 +0330

    first commit

---
+++ lib/constance.ts
@@ -0,0 +1,6 @@
+
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'سامی کالا'
+
+export const APP_SLOGAN =process.env.NEXT_PUBLIC_APP_SLOGAN
+
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION
