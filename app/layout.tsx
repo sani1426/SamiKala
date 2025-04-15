23dd500ff653006816035a6781e8356a60402999
saman <sam.karimi727@gmail.com>
2025-04-16 01:46:30 +0330

    update 2 files, delete 5 files and create 12 files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,21 @@
 import type { Metadata } from "next";
-import { Vazirmatn } from "next/font/google";
+
 import "./globals.css";
 import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from "@/lib/constance";
-
-const vazirmatn = Vazirmatn({
-  variable: "--font-vazir-sans",
-  subsets: ["arabic"],
-});
+import localFont from 'next/font/local'
 
 
+
+
+const myFont = localFont({ src: '../public/font/static/Vazirmatn-Medium.ttf' })
 
 export const metadata: Metadata = {
   title: {
-    template: `${APP_NAME} | %s`,
+    template: `${APP_NAME}  | %s`,
     default: `${APP_NAME}. ${APP_SLOGAN}`,
   },
   description: APP_DESCRIPTION,
-  icons: {
-    icon: '/assets/images/logo.svg',
-  },
+
 }
 
 export default function RootLayout({
@@ -29,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="fa" dir="rtl">
       <body
-        className={`${vazirmatn.variable}`}
+        className={myFont.className}
       >
         {children}
       </body>
