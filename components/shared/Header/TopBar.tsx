b880097367ad263d8036cd477fd2cfeb9561cfdc
saman <sam.karimi727@gmail.com>
2025-04-16 21:57:08 +0330

    update TopBar.tsx

--- components/shared/Header/TopBar.tsx
+++ components/shared/Header/TopBar.tsx
@@ -8,14 +8,14 @@ const TopBar = () => {
     <div className="primary">
         <div className="container mx-auto text-center md:flex-between py-3 px-4">
             <div className="hidden md:flex-center gap-4">
-        <a href="/" className="hover:text-gray-300">
-            <TbBrandMeta className="h-5 w-5" />
+        <a href="/" className="hover:text-gray-300 transition-all duration-300">
+            <TbBrandMeta className="h-6 w-6" />
         </a>
-        <a href="/" className="hover:text-gray-300">
-            <LuInstagram className="h-5 w-5" />
+        <a href="/" className="hover:text-gray-300 transition-all duration-300">
+            <LuInstagram className="h-6 w-6" />
         </a>
-        <a href="/" className="hover:text-gray-300">
-            <RiTwitterXLine className="h-4 w-4" />
+        <a href="/" className="hover:text-gray-300 transition-all duration-300">
+            <RiTwitterXLine className="h-4 w-5" />
         </a>
         </div>
 
@@ -23,7 +23,7 @@ const TopBar = () => {
         <span>ارسال به سراسر کشور - ارسال سریع و قابل اعتماد</span>
         </div>
         <div className="text-sm hidden md:block">
-            <a href="tel:09914318174" className="hover:text-gray-300">09914318174</a>
+            <a href="tel:09914318174" className="hover:text-gray-300 transition-all duration-300">09914318174</a>
         </div>
         </div>
     </div>
