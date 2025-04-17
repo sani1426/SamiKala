5bd5f9bf2ed3ff18c953ae74023784c14554ae25
saman <sam.karimi727@gmail.com>
2025-04-17 22:00:46 +0330

    update SearchBar.tsx

--- components/shared/Header/SearchBar.tsx
+++ components/shared/Header/SearchBar.tsx
@@ -18,14 +18,14 @@ const SearchBar = () => {
   return (
     <div className={`flex-center w-full transition-all duration-300 ${isOpen ? "absolute top-0 right-0  w-full bg-white h-24 z-50" : "w-auto"}`}>
     {isOpen ? (
-        <form className="flex-center relative w-full" >
+        <form className="flex-center relative w-full bg-whit text-black dark:bg-slate-800 dark:bg-slate-100" >
         <div className="relative w-1/2">
-          <input onChange={(e)=> setSearchTerm(e.target.value)} type="text" placeholder="سرچ کنید" value={searchTerm} className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700" />
+          <input onChange={(e)=> setSearchTerm(e.target.value)} type="text" placeholder="سرچ کنید" value={searchTerm} className="bg-gray-200 dark:bg-slate-500 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none w-full placeholder:text-gray-700 dark:placeholder:text-gray-200" />
           <button type="submit" className="absolute  left-2 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800">
             <HiMagnifyingGlass className="w-6 h-6" />
           </button>
         </div>
-        <button type="button" className="left-4 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-800" onClick={()=> setIsOpen(false)}>
+        <button type="button" className="right-4 top-1/2 transform translate-y-1/2 text-gray-600 hover:text-gray-800" onClick={()=> setIsOpen(false)}>
           <HiMiniXMark className="w-6 h-6" />
         </button>
         </form>
