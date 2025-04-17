1dbcb0a6bc2da44c6f53d107134628113e624729
saman <sam.karimi727@gmail.com>
2025-04-18 01:40:57 +0330

    build(deps): update 3 files and create 1 file

--- components/shared/Cart/CartIcon.tsx
+++ components/shared/Cart/CartIcon.tsx
@@ -3,6 +3,14 @@
 import { Button } from '@/components/ui/button'
 import { CartContextType, useCartContext } from '@/context/CartContext'
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+
 import { HiOutlineShoppingBag } from 'react-icons/hi'
 
 const CartIcon = () => {
@@ -10,9 +18,16 @@ const CartIcon = () => {
     const {toggleDrawer} = useCartContext() as CartContextType
   
   return (
+    <TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger>
+    <TooltipContent>سبد خرید</TooltipContent>
     <Button onClick={toggleDrawer} className='text-xl'>
       <HiOutlineShoppingBag className='text-xl h-7 w-7' />
     </Button>
+    </TooltipTrigger>
+  </Tooltip>
+</TooltipProvider>
   )
 }
 
