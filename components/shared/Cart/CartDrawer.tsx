8b9b31cec561476b8757f922c06d52a151ff93f4
saman <sam.karimi727@gmail.com>
2025-04-18 02:30:11 +0330

    update CartDrawer.tsx

--- components/shared/Cart/CartDrawer.tsx
+++ components/shared/Cart/CartDrawer.tsx
@@ -1,32 +1,34 @@
-"use client"
+'use client'
 
-import { Button } from "@/components/ui/button"
-import { CartContextType, useCartContext } from "@/context/CartContext"
-import { IoMdClose } from "react-icons/io"
-import CartContents from "./CartContents"
-
+import { Button } from '@/components/ui/button'
+import { CartContextType, useCartContext } from '@/context/CartContext'
+import { IoMdClose } from 'react-icons/io'
+import CartContents from './CartContents'
 
 const CartDrawer = () => {
-
-    const { drawerOpen , toggleDrawer } = useCartContext() as CartContextType
-
-
+  const { drawerOpen, toggleDrawer } = useCartContext() as CartContextType
 
   return (
-    <div className={`cart-drawer  ${drawerOpen ? "translate-x-0" : "translate-x-full"}`}>
-        <div className="flex-between p-4">
+    <div
+      className={`cart-drawer  ${
+        drawerOpen ? 'translate-x-0' : 'translate-x-full'
+      }`}
+    >
+      <div className='flex-between p-4'>
+        {/* {close button} */}
 
-         { /* {close button} */}
-            <button onClick={toggleDrawer} >
-                <IoMdClose className="h-6 w-6 text-gray-600 hover:text-red-600 dark:text-slate-100 dark:hover:text-red-600" />
-            </button>
-       
-          <h2 className=" font-semibold mb-1 text-center font-beirut text-3xl">سبد خرید</h2>
-        </div>
-        <CartContents />
-        <div className="p-4 sticky bottom-0">
-          <Button  className="w-full py-3 rounded-lg font-semibold">پرداخت</Button>
-        </div>
+        <h2 className=' font-semibold mb-1 text-center font-beirut text-3xl'>
+          سبد خرید
+        </h2>
+
+        <button onClick={toggleDrawer}>
+          <IoMdClose className='h-6 w-6 text-gray-600 hover:text-red-600 dark:text-slate-100 dark:hover:text-red-600' />
+        </button>
+      </div>
+      <CartContents />
+      <div className='p-4 sticky bottom-0'>
+        <Button className='w-full py-3 rounded-lg font-semibold'>پرداخت</Button>
+      </div>
     </div>
   )
 }
