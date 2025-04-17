fb2e34bb6df8f2a8da4be9488a144e49ccf042d6
saman <sam.karimi727@gmail.com>
2025-04-18 03:00:42 +0330

    update CartContents.tsx and constance.ts

--- components/shared/Cart/CartContents.tsx
+++ components/shared/Cart/CartContents.tsx
@@ -1,46 +1,10 @@
 import { Button } from '@/components/ui/button'
+import { cartItem } from '@/lib/constance';
 
 import { RiDeleteBin3Line } from "react-icons/ri";
 
 const CartContents = () => {
-  const cartItem = [
-    {
-      productId: 1,
-      name: 'laptop',
-      screen: 15 / 6,
-      color: 'black',
-      quantity: 1,
-      price: 1500,
-      image: 'https://picsum.photos/200?random=1',
-    },
-    {
-      productId: 2,
-      name: 'mobile',
-      screen: 6,
-      color: 'gold',
-      quantity: 3,
-      price: 2100,
-      image: 'https://picsum.photos/200?random=1',
-    },
-    {
-      productId: 3,
-      name: 'monitor',
-      screen: 21,
-      color: 'silver',
-      quantity: 2,
-      price: 1900,
-      image: 'https://picsum.photos/200?random=1',
-    },
-    {
-      productId: 4,
-      name: 'monitor',
-      screen: 21,
-      color: 'silver',
-      quantity: 2,
-      price: 1900,
-      image: 'https://picsum.photos/200?random=1',
-    },
-  ]
+
 
   return (
     <div className='overflow-y-auto' dir='ltr'>
--- lib/constance.ts
+++ lib/constance.ts
@@ -4,3 +4,71 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'سامی کالا'
 export const APP_SLOGAN =process.env.NEXT_PUBLIC_APP_SLOGAN
 
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION
+
+
+
+export   const cartItem = [
+    {
+      productId: 1,
+      name: 'laptop',
+      screen: 15 / 6,
+      color: 'black',
+      quantity: 1,
+      price: 1500,
+      image: 'https://picsum.photos/200?random=1',
+    },
+    {
+      productId: 2,
+      name: 'mobile',
+      screen: 6,
+      color: 'gold',
+      quantity: 3,
+      price: 2100,
+      image: 'https://picsum.photos/200?random=1',
+    },
+    {
+      productId: 3,
+      name: 'monitor',
+      screen: 21,
+      color: 'silver',
+      quantity: 2,
+      price: 1900,
+      image: 'https://picsum.photos/200?random=1',
+    },
+    {
+      productId: 4,
+      name: 'monitor',
+      screen: 21,
+      color: 'silver',
+      quantity: 2,
+      price: 1900,
+      image: 'https://picsum.photos/200?random=1',
+    },
+    {
+      productId: 5,
+      name: 'monitor',
+      screen: 21,
+      color: 'silver',
+      quantity: 2,
+      price: 1900,
+      image: 'https://picsum.photos/200?random=1',
+    },
+    {
+      productId: 6,
+      name: 'monitor',
+      screen: 21,
+      color: 'silver',
+      quantity: 2,
+      price: 1900,
+      image: 'https://picsum.photos/200?random=1',
+    },
+    {
+      productId: 4,
+      name: 'monitor',
+      screen: 21,
+      color: 'silver',
+      quantity: 2,
+      price: 1900,
+      image: 'https://picsum.photos/200?random=1',
+    },
+  ]
