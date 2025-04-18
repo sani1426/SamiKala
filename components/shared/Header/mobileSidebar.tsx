8759c7b3f0a887c19398e7ee04f0f43f81130d20
saman <sam.karimi727@gmail.com>
2025-04-18 03:31:56 +0330

    update mobileSidebar.tsx

--- components/shared/Header/mobileSidebar.tsx
+++ components/shared/Header/mobileSidebar.tsx
@@ -1,28 +1,33 @@
-"use client"
+'use client'
 
-import { CartContextType, useCartContext } from "@/context/CartContext"
-import {IoMdClose} from "react-icons/io"
-import NavItem from "./NavItem"
-
-
+import { CartContextType, useCartContext } from '@/context/CartContext'
+import { IoMdClose } from 'react-icons/io'
+import NavItem from './NavItem'
+import Link from 'next/link'
+import { IoLogoAlipay } from 'react-icons/io5'
 
 const MobileSidebar = () => {
-
-      const { sidebarOpen, toggleSidebar } = useCartContext() as CartContextType
+  const { sidebarOpen, toggleSidebar } = useCartContext() as CartContextType
   return (
-    <div className={` sidebar-mobile ${sidebarOpen ? "translate-x-0" : "translate-x-full"}` }>
-        <div className="flex justify-end p-4">
-            <button onClick={toggleSidebar}>
-                <IoMdClose className="h-6 w-6  hover:text-red-600 transition-all" />
-            </button>
-        </div>
-        <div className="p-4">
-            <h2 className="text-2xl font-semibold mb-4 font-beirut">منو</h2>
-
-            <nav className="flex flex-col justify-start gap-3">
-                <NavItem />
-            </nav>
-        </div>
+    <div
+      className={` sidebar-mobile ${
+        sidebarOpen ? 'translate-x-0' : 'translate-x-full'
+      }`}
+    >
+      <div className='flex-between  p-4'>
+        <button onClick={toggleSidebar}>
+          <IoMdClose className='h-6 w-6  hover:text-red-600 transition-all' />
+        </button>
+        <Link className='text-2xl flex-center gap-2 text-primary' href='/'>
+          <IoLogoAlipay />
+          <span className='font-beirut text-3xl font-bold'>سامی کالا</span>
+        </Link>
+      </div>
+      <div>
+        <nav className='flex flex-col justify-start gap-3'>
+          <NavItem />
+        </nav>
+      </div>
     </div>
   )
 }
