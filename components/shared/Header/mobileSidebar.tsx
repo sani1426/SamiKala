7672c7a4b2b8a69d464e8b4dacaaff072cd5e21f
saman <sam.karimi727@gmail.com>
2025-04-18 03:34:40 +0330

    update mobileSidebar.tsx

--- components/shared/Header/mobileSidebar.tsx
+++ components/shared/Header/mobileSidebar.tsx
@@ -14,14 +14,16 @@ const MobileSidebar = () => {
         sidebarOpen ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
-      <div className='flex-between  p-4'>
-        <button onClick={toggleSidebar}>
-          <IoMdClose className='h-6 w-6  hover:text-red-600 transition-all' />
-        </button>
-        <Link className='text-2xl flex-center gap-2 text-primary' href='/'>
+      <div className='flex-between  p-4 mb-5'>
+         <Link className='text-2xl flex-center gap-2 text-primary' href='/'>
           <IoLogoAlipay />
           <span className='font-beirut text-3xl font-bold'>سامی کالا</span>
         </Link>
+
+        <button onClick={toggleSidebar}>
+          <IoMdClose className='h-6 w-6  hover:text-red-600 transition-all' />
+        </button>
+       
       </div>
       <div>
         <nav className='flex flex-col justify-start gap-3'>
