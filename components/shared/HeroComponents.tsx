7982d28b089e9ba9773039d4aca4cb6783d0e110
saman <sam.karimi727@gmail.com>
2025-04-19 01:13:43 +0330

    delete 1 file, update 2 files and create 4 files

--- components/shared/HeroComponents.tsx
+++ components/shared/HeroComponents.tsx
@@ -1,36 +1,11 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel'
+import { HomeCarousel } from "./HomeCarousel"
 
-import Card from './Card'
+
+
 
 const HeroComponents = () => {
   return (
-    <Carousel
- 
-    opts={{
-        align: "start",
-        loop: true,
-      }}
-    >
-    <CarouselContent>
-      <CarouselItem className="w-screen h-screen">
-        <Card src='/images/mens-collection.webp' title='لباس مردانه' />
-      </CarouselItem>
-      <CarouselItem className="w-screen h-screen">
-      <Card src='/images/rabbit-hero.webp' title='فروشگاه سامی کالا' />
-      </CarouselItem>
-      <CarouselItem className="w-screen h-screen">
-      <Card src='/images/womens-collection.webp' title='لباس  زنانه' />
-      </CarouselItem>
-    </CarouselContent>
-    <CarouselPrevious />
-      <CarouselNext />
-  </Carousel>
+<HomeCarousel />
   )
 }
 
--- lib/data.ts
+++ lib/data.ts
@@ -5,3 +5,27 @@ export const nav_link = [
     {id : 3 , label : 'ابزار' , href : '/tools'},
     {id : 4 , label : 'موبایل' , href : '/mobile'},
 ]
+
+export const items =  [
+    {
+      title: 'Most Popular Shoes For Sale',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner3.jpg',
+      url: '/search?category=Shoes',
+      isPublished: true,
+    },
+    {
+      title: 'Best Sellers in T-Shirts',
+      buttonCaption: 'Shop Now',
+      image: '/images/banner1.jpg',
+      url: '/search?category=T-Shirts',
+      isPublished: true,
+    },
+    {
+      title: 'Best Deals on Wrist Watches',
+      buttonCaption: 'See More',
+      image: '/images/banner2.jpg',
+      url: '/search?category=Wrist Watches',
+      isPublished: true,
+    },
+  ]
