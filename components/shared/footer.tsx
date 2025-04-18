69813be99035cf5fd462288252b533f324f20e1f
saman <sam.karimi727@gmail.com>
2025-04-18 23:00:09 +0330

    update footer.tsx

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4 lg:px-0">
             <div>
                 <h3 className="text-lg text-slate-800 dark:text-slate-100 mb-4">اطلاع رسانی</h3>
-                <p className="text-gray-500 dark:text-gray-200 mb-4">اولین نفری باشید که در مورد محصولات جدید با خبر میشوید.</p>
-                <p className="font-medium text-sm text-gray-600 dark:text-gray-400">ثبت نام کنید و 10 درصد تخفیف بگیرید</p>
+                <p className="text-gray-500 dark:text-gray-400 mb-4">اولین نفری باشید که در مورد محصولات جدید با خبر میشوید.</p>
+                <p className="font-medium text-sm text-gray-600 dark:text-gray-400 mb-2">ثبت نام کنید و 10 درصد تخفیف بگیرید</p>
                 <form className="flex">
                     <input type="email" placeholder="ایمیل خود را وارد کنید" className="p-3 w-full text-sm border-t border-l border-b border-gray-300 dark:border-gray-700 rounded-r-md focus:outline-none focus:ring-2 focus:ring-gray-500 transition-all" required />
                     <button type="submit" className="px-6 py-3 text-sm rounded-l-md bg-primary  ">ارسال</button>
