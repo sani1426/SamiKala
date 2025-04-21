a7eee479eddd32589d1a96c9c56c0d34dcbbe1d8
saman <sam.karimi727@gmail.com>
2025-04-21 22:09:14 +0330

    update NewArrivalsSlider.tsx

--- components/shared/Products/NewArrivalsSlider.tsx
+++ components/shared/Products/NewArrivalsSlider.tsx
@@ -14,14 +14,14 @@ import Image from "next/image"
 
  function NewArrivalSlider() {
   return (
-    <Carousel className="w-full max-w-sm">
+    <Carousel className="w-full ">
       <CarouselContent className="-ml-1">
         {Array.from({ length: 5 }).map((_, index) => (
           <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
             <div className="p-1">
               <Card>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                 <Image src="banner1.jpg" fill alt="image" />
+                 <Image src="/images/banner1.jpg" fill alt="image" />
                 </CardContent>
               </Card>
             </div>
