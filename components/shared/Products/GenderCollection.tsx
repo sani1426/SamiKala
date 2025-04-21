e0bbedd16104eb8039f1a41c48bbca659bddb4c7
saman <sam.karimi727@gmail.com>
2025-04-21 21:02:05 +0330

    update GenderCollection.tsx

--- components/shared/Products/GenderCollection.tsx
+++ components/shared/Products/GenderCollection.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 const GenderCollection = () => {
   return (
     <section className="py-16 px-4 lg:px-0">
-        <div className="container mx-auto flex flex-col md:flex-row gap-x-80">
+        <div className="container mx-auto flex flex-col md:flex-row gap-x-8">
             <div className="relative flex-1">
                 <Image src='/images/womens-collection.webp' alt="women collection" className="w-full h-[700px] object-cover"  />
                 <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
@@ -16,7 +16,7 @@ const GenderCollection = () => {
                 </div>
             </div>
             <div className="relative flex-1">
-                <Image src='/images/mens-collection.webp' alt="women collection" className="w-full h-[700px] object-cover"  />
+                <Image src='/images/mens-collection.webp' alt="men collection" className="w-full h-[700px] object-cover"  />
                 <div className="absolute bottom-8 left-8 bg-white bg-opacity-90 p-4">
                     <h2 className="text-2xl font-bold text-gray-900 mb-3">
                         mens Collection
