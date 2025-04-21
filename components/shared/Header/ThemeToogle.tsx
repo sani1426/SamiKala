3fd1748f7443b39c976a4cd4e9733eb3d189b074
saman <sam.karimi727@gmail.com>
2025-04-22 01:26:01 +0330

    update ThemeToogle.tsx, NewArrivalsSlider.tsx and dropdown-menu.tsx

--- components/shared/Header/ThemeToogle.tsx
+++ components/shared/Header/ThemeToogle.tsx
@@ -19,20 +19,20 @@ export function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <Sun className="h-[2rem] w-[2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[2rem] w-[2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className='text-center text-primary' align="center">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-        <Sun className="h-[1.2rem] w-[1.2rem]" />
+        <DropdownMenuItem className="text-center" onClick={() => setTheme("light")}>
+        <Sun className="h-[2rem] w-[2rem]" />
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-        <Moon className="h-[1.2rem] w-[1.2rem]" />
+        <DropdownMenuItem className="text-center"  onClick={() => setTheme("dark")}>
+        <Moon className="h-[2rem] w-[2rem]" />
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-        <Settings className="h-[1.2rem] w-[1.2rem]" />
+        <DropdownMenuItem className="text-center"   onClick={() => setTheme("system")}>
+        <Settings className="h-[2rem] w-[2rem]" />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
--- components/shared/Products/NewArrivalsSlider.tsx
+++ components/shared/Products/NewArrivalsSlider.tsx
@@ -1,42 +1,48 @@
+import * as React from 'react'
 
-
-import * as React from "react"
-
-import { Card, CardContent } from "@/components/ui/card"
+import { Card, CardContent } from '@/components/ui/card'
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from '@/components/ui/carousel'
 
-import { NewArrivalItem } from "@/lib/constance"
+import { NewArrivalItem } from '@/lib/constance'
 
- function NewArrivalSlider() {
+function NewArrivalSlider() {
   return (
-    <section className="py-12 px-4 md:px-10 overflow-hidden">
-        <h1 className="text-center text-3xl font-bold my-8">Explore New Arraivals</h1>
-        <p className="text-lg mb-8 text-center">Discover latest styles stright Off the runway, freshly added to keep your wardob on the cutting edge of fashion.</p>
-    <Carousel className="w-full ">
-      <CarouselContent className="-ml-1">
-        {NewArrivalItem.map((_, index) => (
-          <CarouselItem key={index} className="pl-1 md:basis-1/3 lg:basis-1/5">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex  items-center justify-center p-2">
-                <img src={_.images} alt={_.altText}  />
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <section className='py-12 px-4 md:px-10 overflow-hidden'>
+      <h1 className='text-center text-3xl font-bold my-8'>
+        Explore New Arraivals
+      </h1>
+      <p className='text-lg mb-8 text-center'>
+        Discover latest styles stright Off the runway, freshly added to keep
+        your wardob on the cutting edge of fashion.
+      </p>
+      <Carousel className='w-full '>
+        <CarouselContent className='-ml-1'>
+          {NewArrivalItem.map((_, index) => (
+            <CarouselItem
+              key={index}
+              className='pl-1 md:basis-1/3 lg:basis-1/5'
+            >
+              <div className='p-1'>
+                <Card>
+                  <CardContent className='flex  items-center justify-center p-2'>
+                    <img src={_.images} alt={_.altText} />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </section>
   )
 }
 
-export default  NewArrivalSlider
+export default NewArrivalSlider
