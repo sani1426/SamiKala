532c8826f7dd1a49951c9b6b9a41e9f4f216724c
saman <sam.karimi727@gmail.com>
2025-04-23 00:21:43 +0330

    update ProductDetails.tsx

--- components/shared/Products/ProductDetails.tsx
+++ components/shared/Products/ProductDetails.tsx
@@ -21,7 +21,7 @@ const SelectedProduct = {
 
 const ProductDetails = () => {
   return (
-    <div className='p-6'>
+    <div dir="ltr" className='p-6'>
       <div className='max-w-6xl mx-auto bg-white p-8 rounded-lg'>
         <div className='flex flex-col md:flex-row'>
           <div className='hidden md:flex flex-col gap-4 mr-6'>
