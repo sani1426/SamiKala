92592f3bf0c5ba4793f97570231fa2ae09db823e
saman <sam.karimi727@gmail.com>
2025-04-24 00:09:46 +0330

    update NewArrivalsSlider.tsx

--- components/shared/Products/NewArrivalsSlider.tsx
+++ components/shared/Products/NewArrivalsSlider.tsx
@@ -22,29 +22,32 @@ function NewArrivalSlider() {
         your wardob on the cutting edge of fashion.
       </p>
       <Carousel className='w-full '>
-        <CarouselContent className='-ml-1'>
+     
           {NewArrivalItem.map((_, index) => (
+               <CarouselContent       key={index} className='-ml-1 md:basis-1/3 lg:basis-1/4'>
             <CarouselItem
-              key={index}
-              className='px-2 md:basis-1/3 lg:basis-1/4'
+        
+              className='px-2 '
             >
               <div className='p-1'>
                 <Card>
                   <CardContent className='flex  items-center justify-center p-2 relative'>
-    
-                    <img className='w-full h-full' src={_.images} alt={_.altText} />
-                    <div className='flex w-full items-center justify-between absolute bottom-3 px-3'>
-                    <h1 className=''>{_.name}</h1>
-                    <p className='text-sm'>{_.price}</p>
+                    <img
+                      className='w-full h-full'
+                      src={_.images}
+                      alt={_.altText}
+                    />
+                    <div className='flex w-full items-center justify-between absolute bottom-3 px-3 text-white'>
+                      <h1 className=''>{_.name}</h1>
+                      <p className='text-sm'>{_.price}</p>
                     </div>
-                 
-             
                   </CardContent>
                 </Card>
               </div>
             </CarouselItem>
+            </CarouselContent>
           ))}
-        </CarouselContent>
+      
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
