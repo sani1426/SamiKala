3244a00ec79cb4546189f06c2685b40a6b4bdee4
saman <sam.karimi727@gmail.com>
2025-04-24 23:42:32 +0330

    update ProductDetails.tsx

--- components/shared/Products/ProductDetails.tsx
+++ components/shared/Products/ProductDetails.tsx
@@ -80,7 +80,7 @@ const [disable , setDisable] = useState(false)
                   <button
                   onClick={()=> setSelectedSize(size)}
                     key={size}
-                    className={`${selectedSize === size ?  "bg-black text-white dark:bg-white dark:text-black" : ""}  px-4 py-2 rounded border bg-white text-black dark:bg-black dark:text-white`}
+                    className={`${selectedSize === size ?  "bg-black text-white dark:bg-white dark:text-black" : "bg-white text-black dark:bg-black dark:text-white"}  px-4 py-2 rounded border `}
                   >
                     {size}
                   </button>
