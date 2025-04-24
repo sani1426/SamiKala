3f359d69563800eea5dd2ba0cdc83f49e66e0a4f
saman <sam.karimi727@gmail.com>
2025-04-25 03:07:45 +0330

    update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,5 @@
 import GenderCollection from '@/components/shared/Products/GenderCollection'
 import NewArrivalSlider from '@/components/shared/Products/NewArrivalsSlider'
-import {  GetAllProduct } from '@/lib/actions/productActions'
 import BestSeller from '@/section/BestSeller'
 import HeroSection from '@/section/HeroSection'
 import { ProductType } from '@/types/type'
@@ -11,19 +10,14 @@ export default async function Home() {
 
   const {data} = await axios.get("https://sami-kala.vercel.app/api/products")
   const apiproduct : ProductType[] = data
-  const products: ProductType[] = await GetAllProduct()
-  console.log(products)
+  console.log(apiproduct)
   return (
     <main>
       <HeroSection />
       <GenderCollection />
       <NewArrivalSlider />
       <BestSeller />
-      <div>
-        {products.map((item) => (
-          <h1 key={item.name}>{item.name}</h1>
-        ))}
-      </div>
+    
       <div>
         {apiproduct.map((item) => (
           <h1 key={item.name}>{item.name}</h1>
