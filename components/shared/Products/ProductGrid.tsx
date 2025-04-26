f5d40be8341a893be319da9edfb76356043150fb
saman <sam.karimi727@gmail.com>
2025-04-27 01:13:59 +0330

    update ProductGrid.tsx

--- components/shared/Products/ProductGrid.tsx
+++ components/shared/Products/ProductGrid.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const ProductGrid = ({ products }: { products: ProductType[] }) => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cools-2 lg:grid-cols-4 gap-3'>
+    <div className='grid grid-cols-1 sm:grid-cools-2 lg:grid-cols-4 gap-3 gap-y-4'>
       {products.map((product, index) => (
         <Link key={index} href={`/products/${product._id}`} className='block'>
           <div className=' p-4 rounded-lg shadow-lg shadow-slate-800  dark:shadow-slate-300'>
@@ -14,8 +14,8 @@ const ProductGrid = ({ products }: { products: ProductType[] }) => {
                 className='w-full h-full object-cover rounded-lg  hover:opacity-0 transition-all duration-500'
               />
               <img
-                src={product.images[0].url}
-                alt={product.images[0].altText}
+                src={product.images[1].url}
+                alt={product.images[1].altText}
                 className='absolute top-0 w-full h-full object-cover rounded-lg  opacity-0 hover:opacity-[1] transition-all duration-500'
               />
             </div>
