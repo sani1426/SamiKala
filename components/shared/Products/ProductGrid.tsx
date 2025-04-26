055d2644c0d37721f67c3a6b1c1de79834dd4342
saman <sam.karimi727@gmail.com>
2025-04-26 22:40:00 +0330

    update ProductGrid.tsx

--- components/shared/Products/ProductGrid.tsx
+++ components/shared/Products/ProductGrid.tsx
@@ -1,23 +1,26 @@
-import { ProductType } from "@/types/type"
-import Link from "next/link"
+import { ProductType } from '@/types/type'
+import Link from 'next/link'
 
-
-const ProductGrid = ({products} : {products : ProductType[]}) => {
+const ProductGrid = ({ products }: { products: ProductType[] }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cools-2 lg:grid-cols-4 gap-3">
-        {
-            products.map((product , index) => (
-                <Link key={index} href={`/products/${product._id}`} className="block">
-                    <div className=" p-4 rounded-lg shadow-lg shadow-slate-800  dark:shadow-slate-300">
-                        <div className="w-full h-96 mb-4">
-                            <img src={product.images[0].url} alt={product.images[0].altText} className="w-full h-full object-cover rounded-lg" />
-                        </div>
-                        <h3 className="text-sm mb-2">{product.name}</h3>
-                        <p className="text-gray-500 dark:text-gray-400 font-medium text-sm tracking-tighter">$ {product.price}</p>
-                    </div>
-                </Link>
-            ))
-        }
+    <div className='grid grid-cols-1 sm:grid-cools-2 lg:grid-cols-4 gap-3'>
+      {products.map((product, index) => (
+        <Link key={index} href={`/products/${product._id}`} className='block'>
+          <div className=' p-4 rounded-lg shadow-lg shadow-slate-800  dark:shadow-slate-300'>
+            <div className='w-full h-96 mb-4'>
+              <img
+                src={product.images[0].url}
+                alt={product.images[0].altText}
+                className='w-full h-full object-cover rounded-lg'
+              />
+            </div>
+            <h3 className='text-sm mb-2'>{product.name}</h3>
+            <p className='text-gray-500 dark:text-gray-400 font-medium text-sm tracking-tighter'>
+              $ {product.price}
+            </p>
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
